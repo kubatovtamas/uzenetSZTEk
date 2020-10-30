dafa096868ad2a4b19e3b5117dc595f84e4099b9
Kubatov <h665398@stud.u-szeged.hu>
2020-10-30 22:35:48 +0100

    Modify data.sql

--- src/main/java/com/uzenetesztek/controller/ErrorPageController.java
+++ src/main/java/com/uzenetesztek/controller/ErrorPageController.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.ServletWebRequest;
 
