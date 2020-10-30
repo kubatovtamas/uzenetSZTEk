09e4836aea52ce89694cee3eb4af85f78dbc24af
Kubatov <h665398@stud.u-szeged.hu>
2020-10-30 13:15:00 +0100

    Add testPOJO, injection in html, example usage with lombok and thymeleaf

--- src/main/java/com/uzenetesztek/controller/HomeController.java
+++ src/main/java/com/uzenetesztek/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.uzenetesztek.controller;
 
+import com.uzenetesztek.domain.testPOJO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 @Controller
 public class HomeController {
@@ -10,7 +15,18 @@ public class HomeController {
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("testAttribute", "This is a test attribute");
+        model.addAttribute("testPOJOs", getTestPOJOs()); // inject a list of pojos into html
         return "index";
+    }
+
+    private ArrayList<testPOJO> getTestPOJOs() {
+        ArrayList<testPOJO> pojos = new ArrayList<>();
+        testPOJO pojo1 = new testPOJO(1, "macska", "macs@ka.com", new Date(), true);
+        testPOJO pojo2 = new testPOJO(1, "kutya", "ku@tya.com", new Date(),false);
+
+        pojos.add(pojo1);
+        pojos.add(pojo2);
+        return pojos;
     }
 }
 
