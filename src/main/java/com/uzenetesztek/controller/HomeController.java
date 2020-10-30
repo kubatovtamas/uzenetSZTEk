35231bcdffe56f9350d3e83905ee0b72d34c7bca
Kubatov <h665398@stud.u-szeged.hu>
2020-10-30 11:55:00 +0100

    Add Thymeleaf example in index.html. Add namespace declarations in idex.html Add Model example in HomeController.java

--- src/main/java/com/uzenetesztek/controller/HomeController.java
+++ src/main/java/com/uzenetesztek/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.uzenetesztek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("testAttribute", "This is a test attribute");
         return "index";
     }
 }
