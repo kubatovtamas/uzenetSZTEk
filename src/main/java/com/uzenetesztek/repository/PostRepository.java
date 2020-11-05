9eef6642b21db27e0ccd527530d27efb05a7a6db
Kubatov <h665398@stud.u-szeged.hu>
2020-11-05 17:10:02 +0100

    Refactor exceptions/*, repositories

--- src/main/java/com/uzenetesztek/repository/PostRepository.java
+++ src/main/java/com/uzenetesztek/repository/PostRepository.java
@@ -6,7 +6,6 @@ import com.uzenetesztek.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
--- src/main/java/com/uzenetesztek/repository/TopicRepository.java
+++ src/main/java/com/uzenetesztek/repository/TopicRepository.java
@@ -1,13 +1,10 @@
 package com.uzenetesztek.repository;
 
-import com.uzenetesztek.domain.Post;
 import com.uzenetesztek.domain.Topic;
 import com.uzenetesztek.domain.User;
-import lombok.NonNull;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
