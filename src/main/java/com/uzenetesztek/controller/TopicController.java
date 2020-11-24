a65ddee01d0e2b0b591fbfcf99212a44879e51de
Pusztai Ágnes Anna <h984944@stud.u-szeged.hu>
2020-11-24 18:07:08 +0100

    MainController refactored into separate Topic and User controller #42

---
+++ src/main/java/com/uzenetesztek/controller/TopicController.java
@@ -0,0 +1,66 @@
+package com.uzenetesztek.controller;
+
+import com.uzenetesztek.domain.Topic;
+import com.uzenetesztek.exceptions.RecordNotFoundException;
+import com.uzenetesztek.service.PostService;
+import com.uzenetesztek.service.TopicService;
+import com.uzenetesztek.service.TopicWithPostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class TopicController {
+
+    private PostService postService;
+    @Autowired
+    public void setPostService(PostService postServ) {
+        this.postService = postServ;
+    }
+
+    private TopicService topicService;
+    @Autowired
+    public void setTopicService(TopicService topicServ) {
+        this.topicService = topicServ;
+    }
+
+    private TopicWithPostsService topicWithPostsService;
+    @Autowired
+    public void setTopicWithPostsService(TopicWithPostsService topicWithPostsService) { this.topicWithPostsService = topicWithPostsService;
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("topicsWithPosts", topicWithPostsService.getAllTopicsWithPostsOrdered());
+        return "index";
+    }
+
+    @RequestMapping(path = {"/topics", "/topics/{id}"})
+    public String getTopics(@PathVariable("id") Optional<Long> id, Model model) throws RecordNotFoundException {
+        // Specific Topic
+        if (id.isPresent()) {
+
+            Topic topic = topicService.getById(id.get());
+            model.addAttribute("specificTopic", topic);
+            model.addAttribute("posts", postService.getPostsByTopicOrdered(topic));
+
+            return "topic_details";
+
+        }
+        // Every Topic
+        else {
+
+            model.addAttribute("topicsWithPosts", topicWithPostsService.getAllTopicsWithPostsOrdered());
+
+            return "topics";
+        }
+    }
+
+
+}
+
+
---
+++ src/main/java/com/uzenetesztek/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.uzenetesztek.controller;
+
+import com.uzenetesztek.domain.User;
+import com.uzenetesztek.exceptions.RecordNotFoundException;
+import com.uzenetesztek.service.TopicWithPostsService;
+import com.uzenetesztek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userServ) {
+        this.userService = userServ;
+    }
+    
+    private TopicWithPostsService topicWithPostsService;
+    @Autowired
+    public void setTopicWithPostsService(TopicWithPostsService topicWithPostsService) { this.topicWithPostsService = topicWithPostsService;
+    }
+
+    @RequestMapping("/profile")
+    public String profile(Model model) {
+        return "profile";
+    }
+
+    @RequestMapping("/signin")
+    public String signin(Model model) {
+        return "sign_in";
+    }
+
+    @RequestMapping("/user/{id}")
+    public String searchForUser(@PathVariable(value = "id") Long id, Model model) throws RecordNotFoundException {
+        User user = userService.getById(id);
+
+        model.addAttribute("user", user);
+        model.addAttribute("topicsPosted", topicWithPostsService.getAllTopicsWithPostsOrdered(user));
+
+        return "user";
+    }
+}
