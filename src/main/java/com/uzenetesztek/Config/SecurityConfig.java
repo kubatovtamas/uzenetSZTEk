148745afe7d5cfd532e3d8545ea67adace7184cd
Kubatov <h665398@stud.u-szeged.hu>
2020-11-25 14:22:59 +0100

    Modify authorization config in SecurityConfig

--- src/main/java/com/uzenetesztek/Config/SecurityConfig.java
+++ src/main/java/com/uzenetesztek/Config/SecurityConfig.java
@@ -28,22 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.GET, "/")
-                        .permitAll()
-                    .antMatchers(HttpMethod.GET, "/topic/**")
-                        .hasAnyRole("USER", "ADMIN")
-                    .antMatchers(HttpMethod.GET, "/profile")
-                        .hasRole("ADMIN")
-                .and()
-                    .formLogin()
-                        .loginPage("/login")
-                        .permitAll()
-                .and()
-                    .logout()
-                        .logoutSuccessUrl("/login?logout")
-                        .permitAll()
-                ;
-
+            .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/profile").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+            .and()
+                .logout()
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll();
     }
 }
