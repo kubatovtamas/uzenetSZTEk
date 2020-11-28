71c5613e63c927d6b44d3e169269a2253d622592
Pusztai Ágnes Anna <h984944@stud.u-szeged.hu>
2020-11-28 14:25:49 +0100

    Refactored *.sql files as requested #37

---
+++ src/main/resources/sql/create-data-sql.sh
@@ -0,0 +1,14 @@
+cat USER.sql > ../data.sql
+echo "Added USER database"
+
+cat gTOPIC.sql >> ../data.sql
+echo "Added TOPIC database"
+
+cat POST.sql >> ../data.sql
+echo "Added POST database"
+
+cat USER_TOPIC.sql >> ../data.sql
+echo "Added USER_TOPIC database"
+
+cat USER_USER.sql >> ../data.sql
+echo "Added USER_USER database"
